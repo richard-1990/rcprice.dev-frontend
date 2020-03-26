ce1dc3a11fd01a714037934dda000c304ad2da23
Richard Price <r_c_price@hotmail.co.uk>
2020-03-26 18:39:22 +0000

    homepage routing public

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router'
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: () =>
       import('./public/public.module').then(m => m.PublicModule),
   },
--- src/app/public/homepage/homepage.component.ts
+++ src/app/public/homepage/homepage.component.ts
@@ -5,8 +5,4 @@ import { Component, OnInit } from '@angular/core'
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class HomepageComponent {}
