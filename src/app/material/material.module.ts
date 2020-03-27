32a10772a62eed0282aa8d8e90973f76134b454a
Richard Price <r_c_price@hotmail.co.uk>
2020-03-27 18:07:27 +0000

    get some cards in here

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [CommonModule, MatSidenavModule, MatListModule, MatCardModule],
+  exports: [MatSidenavModule, MatListModule, MatCardModule],
 })
 export class MaterialModule {}
