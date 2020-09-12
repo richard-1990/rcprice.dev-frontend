5b6babfcde856c69b66331e84ce326ea5e2f2c53
Richard Price <richard@richards-mbp.lan>
2020-09-13 00:22:30 +0100

    prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,20 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((_, response) => {
-  response.send("Hello from Firebase!");
-});
+  response.send('Hello from Firebase!')
+})
 
 export const blogsCreatedDate = functions.firestore
-  .document("blogs/{blogId}")
+  .document('blogs/{blogId}')
   .onCreate((snap, context) => {
     return snap.ref.set(
       {
         createdAt: admin.firestore.FieldValue.serverTimestamp(),
       },
       { merge: true }
-    );
-  });
+    )
+  })
