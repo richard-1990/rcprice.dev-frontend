7701ebbc8255d09826facf7b57fddcff25ee3597
Richard Price <richard@richards-mbp.lan>
2020-09-13 00:14:15 +0100

    fix baseline test

--- src/app/admin/components/autocomplete-chips/autocomplete-chips.component.spec.ts
+++ src/app/admin/components/autocomplete-chips/autocomplete-chips.component.spec.ts
@@ -1,25 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
 
-import { AutocompleteChipsComponent } from './autocomplete-chips.component';
+import { AutocompleteChipsComponent } from './autocomplete-chips.component'
 
 describe('AutocompleteChipsComponent', () => {
-  let component: AutocompleteChipsComponent;
-  let fixture: ComponentFixture<AutocompleteChipsComponent>;
+  let component: AutocompleteChipsComponent
+  let fixture: ComponentFixture<AutocompleteChipsComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AutocompleteChipsComponent ]
-    })
-    .compileComponents();
-  });
+      imports: [MatAutocompleteModule],
+      declarations: [AutocompleteChipsComponent],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AutocompleteChipsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(AutocompleteChipsComponent)
+    component = fixture.componentInstance
+    component.allItems = []
+    component.items = []
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
