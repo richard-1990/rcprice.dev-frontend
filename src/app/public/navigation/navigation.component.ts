e31ee013fe8bf4c91e2b80df720fbed8ffbc77f4
richard-1990 <r_c_price@hotmail.co.uk>
2020-10-24 17:53:02 +0100

    Feature/tailwind (#338)
    * basics fe
    
    * test SVG
    
    * initially... it works
    
    * tailwind menu
    
    * menu array
    
    * basics fe
    
    * test SVG
    
    * initially... it works
    
    * tailwind menu
    
    * menu array
    
    * audit
    
    * npm update
    
    Co-authored-by: Richard Price <richard@richards-mbp.lan>

--- src/app/public/navigation/navigation.component.ts
+++ src/app/public/navigation/navigation.component.ts
@@ -1,8 +1,35 @@
 import { Component, OnInit } from '@angular/core'
 
+interface MenuItem {
+  title: string
+  routerLink: string
+}
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  menu: MenuItem[] = [
+    {
+      title: 'Home',
+      routerLink: '',
+    },
+    {
+      title: 'About Me',
+      routerLink: 'about-me',
+    },
+    {
+      title: 'Portfolio',
+      routerLink: 'portfolio',
+    },
+    {
+      title: 'Blog',
+      routerLink: 'blog',
+    },
+    {
+      title: 'Contact',
+      routerLink: 'contact',
+    },
+  ]
+}
